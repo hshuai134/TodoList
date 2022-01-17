a44b8db92d88bcd9c95c8573a389c4c2fd3add75
yunjikeji <yunjikeji@yunjikejideMacBook-Pro.local>
2022-01-17 17:06:28 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Table, Button,Space} from 'antd';
+import AddTodo from "./components/AddTodo"
+const { Column, ColumnGroup } = Table;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  state = {
+    items :[],
+    columns : [
+      {
+        title: '名称',
+        dataIndex: 'name',
+      },
+      {
+        title: '内容',
+        dataIndex: 'content',
+      },
+      {
+        title: '截止时间',
+        dataIndex: 'deadline',
+      },
+      {
+        title: '最后修改时间',
+        dataIndex: 'turnoverTime',
+      },
+      {
+        title: '操作',
+        render: () => (
+          <Space size="middle">
+            <a>编辑</a>
+            <a>删除</a>
+          </Space>
+        ),
+      },
+    ],
+  }
+  addTodo(){
+    console.log(1);
+  }
+  componentDidMount(){
+    //存入localStorage
+    // var tempItems = JSON.stringify(this.state.items)
+    // window.localStorage.setItem("itmes",tempItems)
+    
+    //从localStorage中取出
+
+    var tempItems =JSON.parse(window.localStorage.getItem("itmes"));
+    this.setState({items:tempItems})
+  }
+  render() {
+    const {items,columns} = this.state
+    return (
+      <div>
+        <Button onClick={this.addTodo} type="primary">添加代办</Button>
+        <Button>批量删除</Button>
+         <Table rowSelection columns={columns} dataSource={items}  rowKey={items => items.id}>
+          <Space size="middle">
+              <a>Invite</a>
+              <a>Delete</a>
+          </Space>
+         </Table>
+         <AddTodo></AddTodo>
+      </div>
+    )
+  }
 }
-
-export default App;
