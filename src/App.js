ab17a47ca3b7a7daa5a5680bc8a388568b2f63e8
yunjikeji <yunjikeji@yunjikejideMacBook-Pro.local>
2022-01-21 17:26:13 +0800

    完成基本CRUD功能

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
-import {Table,Modal, Button,Space} from 'antd';
-import AddTodo from "./components/AddTodo"
-import EditTodo from "./components/EditTodo"
+import moment from "moment"
+import {Table,Modal,Button,Space,Form, Input,DatePicker} from 'antd';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
+import {nanoid} from "nanoid";
+const { Search } = Input;
 
 export default class App extends Component {
+
+  formRef = React.createRef();
   
   state = {
-    temp:{
-      content:'',
-      deadline:'',
-      id:'',
-      name:''
-    },
+    isOpenEdit:false,
+    temp:null,
     items :[],
     columns : [
       {
@@ -41,50 +40,62 @@ export default class App extends Component {
         render: (text,record) => (
           // this.setState()
           <Space size="middle">
-            <span className="editTodo" onClick={()=>this.openWindow2(record)}>编辑</span>
-            <span className="delete" onClick={()=>this.delConfirm(record.id)}>删除</span>
+            <a className="editTodo" onClick={()=>this.openEditWindow(record)}>编辑</a>
+            <a className="delete" onClick={()=>this.delConfirm(record.id)}>删除</a>
           </Space>
         ),
       },
     ],
-    show1 : true,
-    show2 : true,
-    delKeys:[]
+    delKeys:[],
+    isAddModalVisible:false,
+    isEditModalVisible:false,
+    showItems:null
   }
   //打开添加窗口
-  openWindow1 = () => {
-    this.setState({show1:false})
+  openAddWindow = () => {
+    this.setState({isAddModalVisible:true})
   }
-  //打开编辑窗口自
-  openWindow2 = (record) => {
-    console.log(record.id)
-    this.setState({temp:record})
-    this.setState({show2:false})
+  //打开编辑窗口
+  openEditWindow = (value) => {
+    // console.log(value);
+    this.setState({temp: this.formatFormValues(value),isOpenEdit:true})
+    this.setState({isEditModalVisible:true})
   }
-  //关闭添加窗口
-  closeWindow1 = () => {
-    this.setState({show1:true})
+  formatFormValues = (values) => {
+    values.deadline = moment(values.deadline);
+    return values;
+  };
+  // 关闭添加窗口
+  closeAddWindow = () => {
+    this.setState({isAddModalVisible:false})
   }
   //关闭编辑窗口
-  closeWindow2 = () => {
-    this.setState({show2:true})
+  closeEditWindow = () => {
+    this.setState({isOpenEdit:false})
+    this.setState({isEditModalVisible:false})
   }
   //添加待办
-  addTodo = (item) => {
-    this.setState({items:[...this.state.items,item]});
-    this.closeWindow1();
+  addTodo = async () => {
+    const values = await this.formRef.current.validateFields();
+    // const TempDeadline = moment(values.deadline).format("YYYY-MM-DD HH:mm:ss")
+    // const {name,content,deadline} = values;
+    const newTodo = {id:nanoid(),name:values.name,content:values.content,deadline:moment(values.deadline).format("YYYY-MM-DD HH:mm:ss"),turnoverTime:moment().format("YYYY-MM-DD HH:mm:ss")}
+    this.setState({items:[...this.state.items,newTodo]})
+    // console.log(this.state.items);
+    this.setState({isAddModalVisible:false})
   }
   //编辑待办
-  updateTodo = (item) => {
-    var newData = this.state.items.map((value,i,arr)=>{
-      if(value["id"] === this.state.temp.id){
-        console.log(value["id"],this.state.temp.id);
-        value = item;
-      }
-      return value
+  updateTodo = async () => {
+    const values = await this.formRef.current.validateFields();
+    const todo = this.state.items
+    const newTodo = {id:this.state.temp.id,name:values.name,content:values.content,deadline:moment(values.deadline).format("YYYY-MM-DD HH:mm:ss"),turnoverTime:moment().format("YYYY-MM-DD HH:mm:ss")}
+    console.log("获取到：",values);
+    console.log("修改后的：",newTodo);
+    const newItems =todo.map((val,i,arr) => {
+      return val.id === newTodo.id ? arr[i] = newTodo: val
     })
-    this.setState({items:newData})
-    this.closeWindow2();
+    this.setState({items:newItems})
+    this.closeEditWindow();
   }
   //删除提示框
   delConfirm = (id) => {
@@ -132,7 +143,27 @@ export default class App extends Component {
     })
     this.setState({items:newData});
   }
-
+  //搜索
+  searchTodo = (e) => {
+    console.log(e);
+    this.search(e.trim())
+  }
+  search = (keyword) => {
+    let NewShowItems = this.state.items.filter((val,i,arr) => {
+      let str = val.name;
+      return str.search(keyword)!== -1? val:null;
+    })
+    console.log(NewShowItems);
+    this.setState({showItems:NewShowItems})
+  }
+  keywordChange = (e) => {
+    if(e.target.value.trim() !== ''){
+      this.search(e.target.value.trim())
+    }else{
+      this.setState({showItems:null})
+    }
+    console.log(e.target.value);
+  }
   rowSelection = {
     onChange: (selectedRowKeys, selectedRows) => {
       // var temp = this.state.delKeys
@@ -146,7 +177,6 @@ export default class App extends Component {
       name: record.name,
     }),
   };
-
   componentDidMount(){
     //从localStorage中取出
     var tempItems =JSON.parse(window.localStorage.getItem("itmes"));
@@ -157,16 +187,51 @@ export default class App extends Component {
     var tempItems = JSON.stringify(this.state.items)
     window.localStorage.setItem("itmes",tempItems)
   }
+
   render() {
-    const {items,columns,show1,show2,temp} = this.state
+    
+    const {items,columns,isAddModalVisible,isEditModalVisible,temp,isOpenEdit,showItems} = this.state
     return (
       <div style = {{width:"800px",border:"1px blue solid"}}>
-        <Button  type="primary" onClick={this.openWindow1}>添加代办</Button>
-        <Button onClick={this.delAllconfirm} >批量删除</Button>
-         <Table rowSelection={this.rowSelection} columns={columns} dataSource={items} rowKey={items => items.id}>
+        <Space size = "large">
+          <Space>
+            <Button  type="primary" onClick={this.openAddWindow}>添加代办</Button>
+            <Button onClick={this.delAllconfirm} >批量删除</Button>
+          </Space>
+          <Search size = "middle" onChange = {this.keywordChange} placeholder="input search text" onSearch={this.searchTodo} enterButton />
+        </Space>
+         <Table rowSelection={this.rowSelection} columns={columns} dataSource={showItems?showItems:items} rowKey={items => items.id}>
          </Table>
-         <AddTodo  show = {show1} closeWindow = {this.closeWindow1} addTodo={this.addTodo} ></AddTodo>
-         <EditTodo  show = {show2} todoTemp={temp} closeWindow = {this.closeWindow2} updateTodo={this.updateTodo}></EditTodo>
+
+        <Modal title="添加待办" visible={isAddModalVisible}  onOk={this.addTodo} onCancel={this.closeAddWindow} >
+          <Form ref={this.formRef} className = "addTodoFrom">
+              <Form.Item name='name' label="名称" rules={[{ required: true }]}>
+                  <Input/>
+              </Form.Item>
+              <Form.Item name='content' label="内容" rules={[{ required: true }]}>
+                  <Input/>
+              </Form.Item>
+              <Form.Item name='deadline' label="截止时间">
+              <DatePicker showTime />
+              </Form.Item>
+          </Form>
+        </Modal>
+        {!isOpenEdit ? null : 
+        <Modal title="修改待办" visible={isEditModalVisible} onOk={this.updateTodo} onCancel={this.closeEditWindow} >
+            <Form ref={this.formRef} initialValues={temp}  form={this.form} className = "addTodoFrom">
+                <Form.Item name='name' label="名称" rules={[{ required: true }]}>
+                    <Input/>
+                </Form.Item>
+                <Form.Item  name='content' label="内容" rules={[{ required: true }]}>
+                    <Input/>
+                </Form.Item>
+                <Form.Item name='deadline' label="截止时间">
+                    <DatePicker showTime />
+                    {/* <DatePicker defaultValue = {moment(temp.deadline)} showTime /> */}
+                </Form.Item>
+            </Form>
+          </Modal>
+        }
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import 'antd/dist/antd.min.css';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
